bb415f5add509ce1f16c6ed788fd4fd9a0f3e8d8
ManhThu <hmthu@cmc.com.vn>
2020-06-16 15:51:11 +0900

    update

--- app/src/main/java/com/example/helloworld/RegisterActivity.java
+++ app/src/main/java/com/example/helloworld/RegisterActivity.java
@@ -3,12 +3,26 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    EditText confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+    }
+
+    public void register(EditText confirmPassword) {
+        String eml = email.getText().toString();
+        String pass = password.getText().toString();
     }
 }
