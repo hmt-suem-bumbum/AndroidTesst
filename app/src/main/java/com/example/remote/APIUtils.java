30ba80b0089d106b6dfbac8f08f4481464cf8424
ManhThu <hmthu@cmc.com.vn>
2020-06-22 17:06:39 +0900

    update

--- app/src/main/java/com/example/remote/APIUtils.java
+++ app/src/main/java/com/example/remote/APIUtils.java
@@ -5,7 +5,7 @@ public class APIUtils {
     private APIUtils(){
     };
 
-    public static final String API_URL = "http://192.168.1.84:8000/";
+    public static final String API_URL = "http://10.0.2.2:8000/";
 
     public static UserService getUserService(){
         return RetrofitClient.getClient(API_URL).create(UserService.class);
